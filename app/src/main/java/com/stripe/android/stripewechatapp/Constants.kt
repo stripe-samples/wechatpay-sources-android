b164d70514cf1e8680c7aa1fd1ba0ce26a052446
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2019-09-06 12:43:01 -0400

    Add app (#1)

---
+++ app/src/main/java/com/stripe/android/stripewechatapp/Constants.kt
@@ -0,0 +1,7 @@
+package com.stripe.android.stripewechatapp
+
+internal object Constants {
+    const val APP_ID = "wx65997d6307c3827d"
+    const val APP_SIGNATURE = "5850fa3d9a12b5d23638e99256803483"
+    const val STRIPE_KEY = "pk_live_L4KL0pF017Jgv9hBaWzk4xoB"
+}
