62649f34a2178a97fe6db3c19ce67049d339a9c3
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2019-09-06 14:12:37 -0400

    Update Constants.kt

--- app/src/main/java/com/stripe/android/stripewechatapp/Constants.kt
+++ app/src/main/java/com/stripe/android/stripewechatapp/Constants.kt
@@ -1,7 +1,12 @@
 package com.stripe.android.stripewechatapp
 
 internal object Constants {
-    const val APP_ID = "wx65997d6307c3827d"
-    const val APP_SIGNATURE = "5850fa3d9a12b5d23638e99256803483"
-    const val STRIPE_KEY = "pk_live_L4KL0pF017Jgv9hBaWzk4xoB"
+    // update with your app id
+    const val APP_ID = "my_app_id"
+
+    // update with your app signature
+    const val APP_SIGNATURE = "my_app_signature"
+    
+    // update with your app publishable key
+    const val STRIPE_KEY = "my_key"
 }
