8dfe77181b3d07acb2336d84cd7326daf486791b
Michael Shafrir <mshafrir@stripe.com>
2019-09-11 10:13:24 -0400

    Upgrade to stripe-android v11.1.3

--- app/src/main/java/com/stripe/android/stripewechatapp/BaseWXPayEntryActivity.kt
+++ app/src/main/java/com/stripe/android/stripewechatapp/BaseWXPayEntryActivity.kt
@@ -1,9 +1,11 @@
 package com.stripe.android.stripewechatapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.tencent.mm.opensdk.modelbase.BaseReq
 import com.tencent.mm.opensdk.modelbase.BaseResp
+import com.tencent.mm.opensdk.modelpay.PayResp
 import com.tencent.mm.opensdk.openapi.IWXAPI
 import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler
 import com.tencent.mm.opensdk.openapi.WXAPIFactory
@@ -18,8 +20,20 @@ abstract class BaseWXPayEntryActivity : AppCompatActivity(), IWXAPIEventHandler 
     }
 
     override fun onResp(resp: BaseResp?) {
+        if (resp is PayResp) {
+            val message = when(resp.errCode) {
+                0 -> "Payment successful!"
+                -1 -> "Error during payment"
+                -2 -> "User canceled"
+                else -> "Unknown result"
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 
     override fun onReq(req: BaseReq?) {
+        Toast.makeText(this, "Request type: " + req?.type, Toast.LENGTH_SHORT)
+            .show()
     }
 }
